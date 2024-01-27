2365f16060815d29fc262294c8b91fb98f3c456a
xHarlock <adrianmartinez11199@gmail.com>
2024-01-27 01:16:40 +0100

    push all files

---
+++ 2023-Python/day03/solution.py
@@ -0,0 +1,17 @@
+from utils import file_utils
+
+
+def part_one(input):
+    pass
+
+
+def part_two(input):
+    pass
+
+
+input = file_utils.read_lines('input.txt')
+
+result = part_one(input)
+print(f'Task 1: {result}')
+result = part_two(input)
+print(f'Task 2: {result}')
